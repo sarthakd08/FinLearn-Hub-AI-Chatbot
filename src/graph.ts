6939a62a842f3100b21fd47da2896f628a92828b
Sarthak Dua <sarthakdua@Sarthaks-MacBook-Air.local>
2025-11-06 01:04:46 +0530

    Created Marketing tools and its edges

--- src/graph.ts
+++ src/graph.ts
@@ -3,6 +3,7 @@ import state from "./state.js";
 import { llm } from "./model.js";
 import { getCoursesTool } from "./tools.js";
 import { ToolNode } from "@langchain/langgraph/prebuilt";
+import { AIMessage } from "@langchain/core/messages";
 
 const marketingTools = [getCoursesTool];
 const marketingToolNode = new ToolNode(marketingTools);
@@ -65,19 +66,20 @@ async function frontDeskSupportAgent(stateInput: typeof state.State) {
 
 }
 
-function marketingSupportAgent(stateInput: typeof state.State) {
+async function marketingSupportAgent(stateInput: typeof state.State) {
     console.log("Marketing Support Agent Called");
     const llmWithTools = llm.bindTools(marketingTools);
 
     const SYSTEM_PROMPT = `You are part of the Marketing Team at LearnerHub, an ed-tech company that helps software developers excel in their careers through practical web development and Generative AI courses. You specialize in handling questions about promo codes, discounts, offers, and special campaigns. Answer clearly, concisely, and in a friendly manner. For queries outside promotions (course content, learning), politely redirect the student to the correct team.
             Important: Answer only using given context, else say I don't have enough information about it.
+            Note that marketing tools are available to you, so you can use them to get the information you need.
             `
 
     let trimmedHistory = stateInput.messages as any[];
     if(trimmedHistory.at(-1).type == 'ai') {
         trimmedHistory = trimmedHistory.slice(0, -1)
     }
-    const response = llmWithTools.invoke([
+    const marketingResponse = await llmWithTools.invoke([
         {
             role: "system",
             content: SYSTEM_PROMPT
@@ -85,7 +87,9 @@ function marketingSupportAgent(stateInput: typeof state.State) {
         ...trimmedHistory,
     ])
 
-    return stateInput;
+    return {
+        messages: [marketingResponse]
+    };
 }
 
 function learningSupportAgent(stateInput: typeof state.State) {
@@ -95,8 +99,8 @@ function learningSupportAgent(stateInput: typeof state.State) {
 
 
 
-/* conditional edges */
-function whoIsNext(stateInput: typeof state.State) {
+/* conditional edges for front desk support agent */
+function whoIsNextForFrontDeskSupport(stateInput: typeof state.State) {
     if (stateInput.nextRepresentative.includes("MARKETING")) {
         return "marketingSupport";
     } else if (stateInput.nextRepresentative.includes("LEARNING")) {
@@ -107,6 +111,19 @@ function whoIsNext(stateInput: typeof state.State) {
         return "__end__";
     }
 }
+
+
+function whoIsNextForMarketingSupport(stateInput: typeof state.State) {
+    const lastMessage = stateInput.messages[stateInput.messages.length - 1] as AIMessage;
+
+    if (lastMessage.type == 'ai' && lastMessage.tool_calls && lastMessage.tool_calls.length > 0) {
+        return 'marketingTools';
+    } else {
+        return '__end__';
+    }
+}
+
+
 /**
  * The Graph
  */
@@ -117,16 +134,32 @@ graph.addNode("frontDeskSupport", frontDeskSupportAgent)
         .addNode("marketingSupport", marketingSupportAgent)
         .addNode("learningSupport", learningSupportAgent)
         .addNode("marketingTools", marketingToolNode)
-        .addNode("learningTools", learningToolNode)
+        // .addNode("learningTools", learningToolNode)
         .addEdge("__start__", "frontDeskSupport")
-        .addConditionalEdges("frontDeskSupport", whoIsNext, {
+        .addConditionalEdges("frontDeskSupport", whoIsNextForFrontDeskSupport, {
             marketingSupport: "marketingSupport",
             learningSupport: "learningSupport",
             __end__: "__end__",
         })
         // todo : remove this once after testing
-        .addEdge("marketingSupport", "__end__")
-        .addEdge("learningSupport", "__end__");
+        // .addEdge("marketingSupport", "__end__")
+        .addEdge("learningSupport", "__end__")
+        .addConditionalEdges("marketingSupport", whoIsNextForMarketingSupport, {
+            marketingTools: "marketingTools",
+            __end__: "__end__",
+        })
+        .addEdge("marketingTools", "marketingSupport")
+        // .addConditionalEdges()
+        // .addConditionalEdges("marketingTools", whoIsNext, {
+        //     marketingSupport: "marketingSupport",
+        //     learningSupport: "learningSupport",
+        //     __end__: "__end__",
+        // })
+        // .addConditionalEdges("learningTools", whoIsNext, {
+        //     marketingSupport: "marketingSupport",
+        //     learningSupport: "learningSupport",
+        //     __end__: "__end__",
+        // })
 
 const app = graph.compile();
 
@@ -136,7 +169,8 @@ const main = async () => {
         messages: [
             {
                 role: "user",
-                content: "Hi,I need to know about the courses which i can take to improve my skills as 7 years experienced developer?",
+                // content: "Hi,I need to know about the courses which i can take to improve my skills as 7 years experienced developer?", // Should route to learning support agent
+                content: "Hi, do you have any offers going on?" // Should route to marketing support agent
             },
         ],
     });
